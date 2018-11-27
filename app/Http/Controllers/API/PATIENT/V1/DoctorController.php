18d1ed799111828d19f7d264e47f45dc5a2c0276
phamhieu <hieuphamminh96@gmail.com>
2018-11-27 19:25:40 +0700

    api test detail doctor for patient

--- app/Http/Controllers/API/PATIENT/V1/DoctorController.php
+++ app/Http/Controllers/API/PATIENT/V1/DoctorController.php
@@ -53,4 +53,55 @@ class DoctorController extends Controller
             ]
             );
     }
+
+    public function detail($idDoctor)
+    {
+        return [
+            'code'=>200,
+            'status'=>'success',
+            'data'=>
+                    [
+
+                  "status"=> 0|1|2,
+                  "avatar"=> "abc.com",
+                  "rate"=> 2,
+                  "count_rate"=> 100,
+                  "doctor"=> [
+                    "hospital_name"=> "Bệnh viện Vđ",
+                    "gender"=> "Nam",
+                    "experience"=> "30 năm kinh nghiệm",
+                    "position"=> "Giám đốc",
+                    "description"=> "Mô tả"
+                  ],
+                  "plans"=> [
+                    [ "clinic_name"=> "tên phòng khám",
+                      "price"=> 300000,
+                      "status"=> 1,
+                      "day"=> 0-6,
+                      "startHour" => 8,
+                      "endHour" => 9,
+
+                    ],
+                      [ "clinic_name"=> "tên phòng khám",
+                          "price"=> 300000,
+                          "status"=> 1,
+                          "day"=> 0-6,
+                          "startHour" => 8,
+                          "endHour" => 9,
+
+                      ],
+                  ],
+                  "specialties"=> [
+                    [ "id"=> 1, "name"=> "Răng hàm mặt" ],
+                    ["id"=> 2, "name"=> "Đỡ đẻ" ]
+                  ],
+                  "clinics"=> [
+                    [ "id"=> 1, "name"=> "phong kham 1" ],
+                    ["id"=> 2, "name"=> "phong kham 2" ]
+                  ]
+                ]
+
+        ];
+
+    }
 }
