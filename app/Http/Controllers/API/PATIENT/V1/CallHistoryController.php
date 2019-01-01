b408c0321380e0914a890c0b91d9b5c452633088
phamhieu <hieuphamminh96@gmail.com>
2019-01-01 16:02:44 +0700

    api create call history

---
+++ app/Http/Controllers/API/PATIENT/V1/CallHistoryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\PATIENT\V1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CallHistoryController extends Controller
+{
+    public function store(\App\Http\Requests\API\V1\Request $request)
+    {
+
+    }
+}
