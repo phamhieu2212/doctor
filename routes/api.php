0015d1e5285685017ebfe841e4a857c010692753
GiapHV-TTJ <giaphv@ttjworld.com>
2019-01-01 15:53:37 +0700

    patient add point

--- routes/api.php
+++ routes/api.php
@@ -76,6 +76,10 @@ Route::group(['namespace' => 'API'], function () {
                 Route::get('/order', 'PlanController@order');
 
             });
+            Route::group(['prefix' => 'point'], function() {
+                Route::post('add', 'PatientController@addPoint');
+            });
+
             Route::post('signout', 'AuthController@postSignOut');
         });
     });
