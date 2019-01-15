79bf3c02fc505a935ec4433ea73f5b9118dc48c9
phamhieu <hieuphamminh96@gmail.com>
2019-01-15 15:45:34 +0700

    fix api search doctor

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -101,7 +101,7 @@ class Doctor extends Base
             'name' => ($this->name)?$this->name:"",
             'hospital_name' => $this->hospital['name'],
             'position' => ($this->position)?$this->position:"",
-            'image_link' => (!empty($this->adminUser->present()->profileImage()))?$this->adminUser->present()->profileImage()->present()->url: \URLHelper::asset('img/no_image.jpg', 'common'),
+            'image_link' => ($this->adminUser->present()->profileImage() != 0)?$this->adminUser->present()->profileImage()->present()->url: \URLHelper::asset('img/no_image.jpg', 'common'),
         ];
     }
 
