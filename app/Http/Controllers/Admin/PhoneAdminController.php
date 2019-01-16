588df713e1d30d0961de264aa093ca9271184368
phamhieu <hieuphamminh96@gmail.com>
2019-01-16 16:29:07 +0700

    test

--- app/Http/Controllers/Admin/PhoneAdminController.php
+++ app/Http/Controllers/Admin/PhoneAdminController.php
@@ -39,7 +39,7 @@ class PhoneAdminController extends Controller
             $filter['query'] = $keyword;
         }
 
-        $count = $this->phoneAdminRepository->countByFilter($filter);dd($count);
+        $count = $this->phoneAdminRepository->countByFilter($filter);
         $phoneAdmins = $this->phoneAdminRepository->getByFilter($filter, $paginate['order'], $paginate['direction'], $paginate['offset'], $paginate['limit']);
 
         return view(
