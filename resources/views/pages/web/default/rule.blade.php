86f2b860ab5ca6830cb468b3380febd8174536fd
phamhieu <hieuphamminh96@gmail.com>
2019-01-18 11:21:11 +0700

    view rule

---
+++ resources/views/pages/web/default/rule.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Điều khoản </title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="stylesheet" href="{!! \URLHelper::asset('libs/bootstrap/css/bootstrap.min.css', 'admin') !!}">
+    <link rel="stylesheet" href="{!! \URLHelper::asset('default/css/custom.css', 'web') !!}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+</head>
+<body>
+<div class="group-background">
+    <div class="container">
+        <div class="col-md-12">
+            <h4 class="text-left title-content"><strong>Điều khoản sử dụng</strong></h1>
+                <div class="content">
+                    <p>Cảm ơn bạn đã sử dụng các sản phẩm và dịch vụ của chúng tôi (“Dịch vụ”). Dịch vụ được cung cấp bởi Google LLC (“Google”) có trụ sở tại 1600 Amphitheatre Parkway, Mountain View, CA 94043, Hoa Kỳ.</p>
+                    <p>Bằng việc sử dụng Dịch vụ của chúng tôi, bạn đang đồng ý với các điều khoản này. Vui lòng đọc kỹ các điều khoản này.</p>
+                    <p>Dịch vụ của chúng tôi rất đa dạng nên đôi khi có thể áp dụng các điều khoản bổ sung hoặc các yêu cầu về sản phẩm (bao gồm cả yêu cầu về độ tuổi). Các điều khoản bổ sung sẽ được cung cấp kèm theo Dịch vụ liên quan và các điều khoản bổ sung đó sẽ trở thành một phần thuộc thỏa thuận của bạn với chúng tôi nếu bạn sử dụng các Dịch vụ đó.</p>
+                </div>
+        </div>
+    </div>
+    <!-- <div class="footer-content"> -->
+</div>
+</div>
+<script src="{!! \URLHelper::asset('default/js/jquery.js', 'web') !!}"></script>
+<script src="{!! \URLHelper::asset('libs/bootstrap/js/bootstrap.min.js', 'admin') !!}"></script>
+</body>
+</html>
