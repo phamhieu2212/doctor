5bfbda41c3b02a00702db80a011f0dae657a4dfb
phamhieu <hieuphamminh96@gmail.com>
2019-01-22 16:37:01 +0700

    fix response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -53,7 +53,7 @@ class Handler extends ExceptionHandler
             }
             if($exception->getMessage() == "Unauthenticated.")
             {
-                return ['code' => 401, 'message' => 'Tài khoản của bạn đã đăng nhập trên thiết bị khác, vui lòng đăng nhập lại', 'data' => null];
+                return response()->json(['code' => 401, 'message' => 'Tài khoản của bạn đã đăng nhập trên thiết bị khác, vui lòng đăng nhập lại', 'data' => null])->setStatusCode(401);
             };
 
             if( !$exception->getMessage() ) {
