3a86255699cf2095fe2b9c7016418ec4d1e05583
phamhieu <hieuphamminh96@gmail.com>
2019-02-16 03:35:40 +0700

    fix name

--- app/Http/Controllers/API/PATIENT/V1/HistoryController.php
+++ app/Http/Controllers/API/PATIENT/V1/HistoryController.php
@@ -23,7 +23,7 @@ class HistoryController extends Controller
     public function index()
     {
 
-        $idPatient  = $this->userService->getUser()->id;
+        $idPatient  = $this->userService->getUser()->id;dd($idPatient);
         $callHistories = CallHistory::where('start_time','!=',null)
             ->where('user_id',$idPatient)->get();
         $chatHistories = ChatHistory::where('user_id',$idPatient)->get();
