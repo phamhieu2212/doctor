1b3cce3252b1b5d31531ed05d01a570b222e735a
phamhieu <hieuphamminh96@gmail.com>
2019-02-19 16:44:55 +0700

    fix upload ava quick

--- app/Http/Controllers/API/DOCTOR/V1/ImageUploadController.php
+++ app/Http/Controllers/API/DOCTOR/V1/ImageUploadController.php
@@ -61,12 +61,25 @@ class ImageUploadController extends Controller
                 }
 
                 $this->adminUserRepository->update($adminUser, ['profile_image_id' => $newImage->id]);
+                $adminUser = $this->adminUserRepository->find($idDoctor);
+                $flag = true;
+                $try = 1;
+                while ($flag && $try <= 3):
+                    try {
+                        Image::make($adminUser->present()->profileImage()->present()->url)->encode('jpg')->save('static/common/img/quick-ava/'.$adminUser->quick_id.'.jpg');
+                        //Image migrated successfully
+                        $flag = false;
+                    } catch (\Exception $e) {
+                        //not throwing  error when exception occurs
+
+                    }
+                    $try++;
+                endwhile;
 
 
             }
         }
         $adminUser = $this->adminUserRepository->find($idDoctor);
-        Image::make($adminUser->present()->profileImage()->present()->url)->encode('jpg')->save('static/common/img/quick-ava/'.$adminUser->quick_id.'.jpg');
 
         return Response::response(200, $adminUser->toAPIArrayUploadAvatar());
 
