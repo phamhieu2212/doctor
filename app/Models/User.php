fed5867a428f8318f92e260d70e4ff104c0ed22a
phamhieu <hieuphamminh96@gmail.com>
2019-02-20 18:44:14 +0700

    fix ava user

--- app/Models/User.php
+++ app/Models/User.php
@@ -155,7 +155,7 @@ class User extends AuthenticatableBase
             'name'      => $this->patient->full_name,
             'age'  => ($this->patient->birth_day != null)?date_diff(date_create($this->patient->birth_day), date_create('now'))->y:null,
             'address'   => $this->address,
-            'avatar'    => !empty($this->present()->profileImage()) ? $this->present()->profileImage()->url : \URLHelper::asset('img/user_avatar.png', 'common'),
+            'avatar'    => !empty($this->patient->present()->profileImage()) ? $this->present()->profileImage()->url : \URLHelper::asset('img/user_avatar.png', 'common'),
         ];
     }
 
